95a8b6547a902692ec865833b6bc33713caa75b9
AndyV <you@example.com>
2023-08-03 13:43:13 -0700

    Users

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
 import Product from "../Models/productModel.js";
 import data from "../data.js";
+import User from "../Models/userModel.js";
 
 const seedRouter = express.Router();
 
 seedRouter.get("/", async (req, res) => {
   await Product.remove({});
   const createdProducts = await Product.insertMany(data.products);
-  res.send({ createdProducts });
+  await User.remove({});
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdProducts, createdUsers });
 });
 export default seedRouter;
